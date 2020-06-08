08417cb43ca83cf4b7081b7417bc953cdb770c82
DominiqueOyco <dominickoyco@gmail.com>
2020-06-08 16:10:36 -0700

    Update mainAccount.py

--- mainAccount.py
+++ mainAccount.py
@@ -11,7 +11,51 @@ import tkinter as tk
 window = tk.Tk()
 
 class newUser():
-    print("punk'd")
+    window.title('Maze Bank of Los Santos')
+    
+    canvas = tk.Canvas(window, height=60, width=720)
+    canvas.pack()
+    
+    # Gets the requested values of the height and widht.
+    windowWidth = window.winfo_reqwidth()
+    windowHeight = window.winfo_reqheight()
+    
+    # Gets both half the screen width/height and window width/height
+    positionRight = int(window.winfo_screenwidth()/2 - windowWidth/2)
+    positionDown = int(window.winfo_screenheight()/2 - windowHeight/2)
+    
+    #window.geometry("720x480+10+20")
+    window.geometry("720x480+{}+{}".format(positionRight, positionDown))
+    window.resizable(0,0)
+    
+    #When user clicks the buttons    
+    newUsrbtn=tk.Button(window, text="CREATE NEW ACCOUNT", fg='blue', 
+                     font=("Avenir", 16))
+    newUsrbtn.place(x=268, y=290)
+    
+    homebtn=tk.Button(window, text="BACK TO LOGIN", fg='blue', 
+                      font=("Avenir", 16))
+    homebtn.place(x=295, y=330)
+    
+    extbtn=tk.Button(window, text="EXIT", fg='blue', font=("Avenir", 16), 
+                     command=window.quit)
+    extbtn.place(x=338, y=370)
+    
+    lbl=tk.Label(window, text="Welcome to the Maze Bank of Los Santos", 
+              fg='red', font=("Avenir", 24))
+    lbl.place(x=140, y=50)
+    
+    #Input validation
+    usrent=tk.Entry(window, text="Access ID", fg='red', font=("Avenir", 16))
+    usrent.place(x=260, y=130)
+    passent=tk.Entry(window, text="Password", fg='red', font=("Avenir", 16))
+    passent.place(x=260, y=210)
+    
+    
+    cpylbl=tk.Label(window, text="© 2020 Maze Bank Inc. All Rights Reserved.", 
+                 fg='red', font=("Avenir", 10))
+    cpylbl.place(x=250, y=420)
+    window.mainloop()
     
 class balance():
 #    TODO: Implement this function
@@ -33,71 +77,75 @@ class savings():
 #    TODO: Implement this function
     print("punk'd")
 
-class mainGUI():
-    window.title('Maze Bank of Los Santos')
+#class mainGUI():
+#    window.title('Maze Bank of Los Santos')
+#    
+#    canvas = tk.Canvas(window, height=60, width=720)
+#    canvas.pack()
+#    
+#    #img = ImageTk.PhotoImage(Image.open("LSBank-GTAV.png"))  
+#    #l=tk.Label(image=img)
+#    #l.pack()
+#    
+##    path = "maze-bank-logo.png"
+##    logo_width = 320
+##    logo_height = 320
+#    
+#    ##Creates a Tkinter-compatible photo image, which can be used everywhere Tkinter expects an image object.
+#    #img = ImageTk.PhotoImage(Image.open(path))
+#    #
+#    #scale_w = 320/40
+#    #scale_h = 320/40
+#    #img._PhotoImage__photo.zoom(scale_w, scale_h)
+#    #
+#    ##The Label widget is a standard Tkinter widget used to display a text or image on the screen.
+#    #panel = tk.Label(window, image = img)
+#    #
+#    ##The Pack geometry manager packs widgets in rows or columns.
+#    #panel.pack(side = "bottom", fill = "both", expand = "yes")
+#    
+#    # Gets the requested values of the height and widht.
+#    windowWidth = window.winfo_reqwidth()
+#    windowHeight = window.winfo_reqheight()
+#    
+#    # Gets both half the screen width/height and window width/height
+#    positionRight = int(window.winfo_screenwidth()/2 - windowWidth/2)
+#    positionDown = int(window.winfo_screenheight()/2 - windowHeight/2)
+#    
+#    #window.geometry("720x480+10+20")
+#    window.geometry("720x480+{}+{}".format(positionRight, positionDown))
+#    window.resizable(0,0)
+#    
+#    #When user clicks the buttons
+#    entbtn=tk.Button(window, text="SIGN IN", fg='blue', font=("Avenir", 16))
+#    entbtn.place(x=328, y=290)
+#    
+#    newUsrbtn=tk.Button(window, text="CREATE NEW ACCOUNT", fg='blue', 
+#                     font=("Avenir", 16))
+#    newUsrbtn.place(x=268, y=330)
+#    
+#    extbtn=tk.Button(window, text="EXIT", fg='blue', font=("Avenir", 16), 
+#                     command=window.quit)
+#    extbtn.place(x=338, y=370)
+#    
+#    lbl=tk.Label(window, text="Welcome to the Maze Bank of Los Santos", 
+#              fg='red', font=("Avenir", 24))
+#    lbl.place(x=140, y=50)
+#    
+#    #Input validation
+#    usrent=tk.Entry(window, text="Access ID", fg='red', font=("Avenir", 16))
+#    usrent.place(x=260, y=130)
+#    passent=tk.Entry(window, text="Password", fg='red', font=("Avenir", 16))
+#    passent.place(x=260, y=210)
+#    
+#    
+#    cpylbl=tk.Label(window, text="© 2020 Maze Bank Inc. All Rights Reserved.", 
+#                 fg='red', font=("Avenir", 10))
+#    cpylbl.place(x=250, y=420)
+#    window.mainloop()
     
-    canvas = tk.Canvas(window, height=60, width=720)
-    canvas.pack()
-    
-    #img = ImageTk.PhotoImage(Image.open("LSBank-GTAV.png"))  
-    #l=tk.Label(image=img)
-    #l.pack()
-    
-    path = "maze-bank-logo.png"
-    logo_width = 320
-    logo_height = 320
-    
-    ##Creates a Tkinter-compatible photo image, which can be used everywhere Tkinter expects an image object.
-    #img = ImageTk.PhotoImage(Image.open(path))
-    #
-    #scale_w = 320/40
-    #scale_h = 320/40
-    #img._PhotoImage__photo.zoom(scale_w, scale_h)
-    #
-    ##The Label widget is a standard Tkinter widget used to display a text or image on the screen.
-    #panel = tk.Label(window, image = img)
-    #
-    ##The Pack geometry manager packs widgets in rows or columns.
-    #panel.pack(side = "bottom", fill = "both", expand = "yes")
-    
-    # Gets the requested values of the height and widht.
-    windowWidth = window.winfo_reqwidth()
-    windowHeight = window.winfo_reqheight()
-    
-    # Gets both half the screen width/height and window width/height
-    positionRight = int(window.winfo_screenwidth()/2 - windowWidth/2)
-    positionDown = int(window.winfo_screenheight()/2 - windowHeight/2)
-    
-    #window.geometry("720x480+10+20")
-    window.geometry("720x480+{}+{}".format(positionRight, positionDown))
-    window.resizable(0,0)
-    
-    #When user clicks the buttons
-    entbtn=tk.Button(window, text="SIGN IN", fg='blue', font=("Avenir", 16))
-    entbtn.place(x=328, y=290)
-    entbtn=tk.Button(window, text="CREATE NEW ACCOUNT", fg='blue', 
-                     font=("Avenir", 16))
-    entbtn.place(x=268, y=330)
-    extbtn=tk.Button(window, text="EXIT", fg='blue', font=("Avenir", 16), 
-                     command=window.quit)
-    extbtn.place(x=338, y=370)
-    lbl=tk.Label(window, text="Welcome to the Maze Bank of Los Santos", 
-              fg='red', font=("Avenir", 24))
-    lbl.place(x=140, y=50)
-    
-    #Input validation
-    usrent=tk.Entry(window, text="Access ID", fg='red', font=("Avenir", 16))
-    usrent.place(x=260, y=130)
-    passent=tk.Entry(window, text="Password", fg='red', font=("Avenir", 16))
-    passent.place(x=260, y=210)
-    
-    
-    cpylbl=tk.Label(window, text="© 2020 Maze Bank Inc. All Rights Reserved.", 
-                 fg='red', font=("Avenir", 10))
-    cpylbl.place(x=250, y=420)
-    window.mainloop()
-    
-mainGUI()
+#mainGUI()
+newUser()
 
 
 
